a98d6b5dcbe0bf15cccceb07db8d93b467e390b4
Anthony Scott <ascott329@gmail.com>
2018-01-16 11:09:00 -0600

    incomplete, need a way to keep bin name with matrix

--- CalculateCompleteBins.py
+++ CalculateCompleteBins.py
@@ -2,6 +2,7 @@ from ParseBam import BamFileReadParser
 import sys
 import os
 import logging
+from multiprocessing import Pool
 import numpy as np
 import pandas as pd
 from sklearn.cluster import DBSCAN
@@ -21,6 +22,21 @@ from sklearn.metrics import silhouette_samples, silhouette_score
 ##########################
 
 
+def CalculateBinCoverage(bin):
+    parser = BamFileReadParser(input_bam_file, 20)
+    try:
+        reads = parser.parse_reads(chromosome, start_pos, stop_pos)
+        matrix = parser.create_matrix(reads)
+    except ValueError:
+        # No reads in this window
+        logging.info("No reads found in the window of {} to {}".format(start_pos, stop_pos))
+        continue
+
+    matrix = matrix.dropna()
+
+    return matrix
+
+
 # Input params
 # todo add these as command line args using argparse
 input_bam_file = sys.argv[1]
@@ -38,35 +54,29 @@ logging.info("Input params, bin size: {}".format(bin_size))
 logging.info("Chromosome: {}".format(chromosome))
 
 # Setup parser object on the bam file
-parser = BamFileReadParser(input_bam_file, 20)
 chrom_lengths = dict(zip(parser.OpenBamFile.references, parser.OpenBamFile.lengths))
 
 # Open output file for writing
 output_file = open(os.path.join(BASE_DIR, output_filename), 'w')
 #output_file.write("chromosome,start,stop,full_reads,CpGs\n")
 
+
+# generate all bins for chromosome
+bins = list(np.arange(bin_size, chrom_lengths[chromosome] + bin_size, bin_size))
+# convert to input format
+bins = ["{}_".format(chromosome) + str(x) for x in bins]
+
 # Start looping over the bam file
-current_bin = 0
-while current_bin <= chrom_lengths[chromosome]:
-    current_bin = current_bin + bin_size
-    stop_pos = current_bin
-    start_pos = stop_pos - bin_size
-    try:
-        reads = parser.parse_reads(chromosome, start_pos, stop_pos)
-        matrix = parser.create_matrix(reads)
-    except ValueError:
-        # No reads in this window
-        logging.info("No reads found in the window of {} to {}".format(start_pos, stop_pos))
-        continue
+    pool = Pool(processes=4)
+    results = pool.map(CalculateBinCoverage, bins)
 
-    # Drop reads with empty values
-    matrix = matrix.dropna()
-
-    output_file.write(chromosome + ",")
-    output_file.write(str(start_pos) + ",")
-    output_file.write(str(stop_pos) + ",")
-    output_file.write(str(matrix.shape[0]) + ",")
-    output_file.write(str(matrix.shape[1]) + "\n")
+    for result in results:
+        # todo bin must be linked with matrix output maybe create dict above
+        output_file.write(chromosome + ",")
+        output_file.write(str(start_pos) + ",")
+        output_file.write(str(stop_pos) + ",")
+        output_file.write(str(result.shape[0]) + ",")
+        output_file.write(str(result.shape[1]) + "\n")
 
 
 output_file.close()
