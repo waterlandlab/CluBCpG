7b692666903647a388e67aa13a48668d66082b2f
Anthony Scott <ascott329@gmail.com>
2018-01-23 14:46:24 -0600

    added __main__ function

--- CalculateCompleteBins.py
+++ CalculateCompleteBins.py
@@ -37,47 +37,50 @@ def CalculateBinCoverage(bin):
     return matrix
 
 
-# Input params
-# todo add these as command line args using argparse
-input_bam_file = sys.argv[1]
-bin_size = 100
-chromosome = 'chr19'
-log_file = "CalculateCompleteBins.{}.log".format(os.path.basename(input_bam_file))
-output_filename = "CalculateCompleteBins.{}.csv".format(os.path.basename(input_bam_file))
-BASE_DIR = os.path.dirname(input_bam_file)
+if __name__ == "__main__":
 
-logging.basicConfig(filename=os.path.join(BASE_DIR, log_file), level=logging.DEBUG)
+    # Input params
+    # todo add these as command line args using argparse
+    input_bam_file = sys.argv[1]
+    bin_size = 100
+    chromosome = 'chr19'
+    log_file = "CalculateCompleteBins.{}.log".format(os.path.basename(input_bam_file))
+    output_filename = "CalculateCompleteBins.{}.csv".format(os.path.basename(input_bam_file))
+    BASE_DIR = os.path.dirname(input_bam_file)
 
-
-logging.info("Input file is {}".format(input_bam_file))
-logging.info("Input params, bin size: {}".format(bin_size))
-logging.info("Chromosome: {}".format(chromosome))
-
-# Setup parser object on the bam file
-chrom_lengths = dict(zip(parser.OpenBamFile.references, parser.OpenBamFile.lengths))
-
-# Open output file for writing
-output_file = open(os.path.join(BASE_DIR, output_filename), 'w')
-#output_file.write("chromosome,start,stop,full_reads,CpGs\n")
+    logging.basicConfig(filename=os.path.join(BASE_DIR, log_file), level=logging.DEBUG)
 
 
-# generate all bins for chromosome
-bins = list(np.arange(bin_size, chrom_lengths[chromosome] + bin_size, bin_size))
-# convert to input format
-bins = ["{}_".format(chromosome) + str(x) for x in bins]
+    logging.info("Input file is {}".format(input_bam_file))
+    logging.info("Input params, bin size: {}".format(bin_size))
+    logging.info("Chromosome: {}".format(chromosome))
 
-# Start looping over the bam file
-    pool = Pool(processes=4)
-    results = pool.map(CalculateBinCoverage, bins)
+    # Setup parser object on the bam file
+    chrom_lengths = dict(zip(parser.OpenBamFile.references, parser.OpenBamFile.lengths))
 
-    for result in results:
-        # todo bin must be linked with matrix output maybe use temp files and merge
-        output_file.write(chromosome + ",")
-        output_file.write(str(start_pos) + ",")
-        output_file.write(str(stop_pos) + ",")
-        output_file.write(str(result.shape[0]) + ",")
-        output_file.write(str(result.shape[1]) + "\n")
+    # Open output file for writing
+    output_file = open(os.path.join(BASE_DIR, output_filename), 'w')
+    #output_file.write("chromosome,start,stop,full_reads,CpGs\n")
 
 
-output_file.close()
-logging.info("Complete")
+    # generate all bins for chromosome
+    bins = list(np.arange(bin_size, chrom_lengths[chromosome] + bin_size, bin_size))
+    # convert to input format
+    bins = ["{}_".format(chromosome) + str(x) for x in bins]
+
+    # Start looping over the bam file
+        pool = Pool(processes=4)
+        results = pool.map(CalculateBinCoverage, bins)
+
+        for result in results:
+            # todo bin must be linked with matrix output maybe use temp files and merge
+            output_file.write(chromosome + ",")
+            output_file.write(str(start_pos) + ",")
+            output_file.write(str(stop_pos) + ",")
+            output_file.write(str(result.shape[0]) + ",")
+            output_file.write(str(result.shape[1]) + "\n")
+
+
+    output_file.close()
+    logging.info("Complete")
+
