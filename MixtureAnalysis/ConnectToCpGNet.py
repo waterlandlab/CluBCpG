551a63ee3f82173741f7dd4cc2d44a281dc3f20b
Anthony Scott <anthony@canthonyscott.com>
2018-10-15 13:55:20 -0500

    setup for training

--- MixtureAnalysis/ConnectToCpGNet.py
+++ MixtureAnalysis/ConnectToCpGNet.py
@@ -1,7 +1,8 @@
-from CpGNet import CpGNet, TRAINED_CPG_MODELS
+from CpGNet import CpGNet
 import numpy as np
 from keras.models import load_model
 import keras.backend as K
+import os
 
 class ImputeWithCpGNet:
 
@@ -55,4 +56,44 @@ class ImputeWithCpGNet:
         return predicted_matrix
 
 
-class Train
+class TrainWithCpGNet:
+
+    def __init__(self, cpg_density=None, save_path=None):
+        """
+        Class to train a CpGNet model from input data
+        :param cpg_density: Number of CpGs
+        :param save_path: Location of folder to save the resulting model files. One per cpg density
+        """
+        if not cpg_density:
+            raise AttributeError("CpG density must be specified")
+        if not save_path:
+            raise AttributeError("Folder to save trained model must be specified")
+        self.save_path = save_path
+        self.cpg_density = cpg_density
+        self.net = CpGNet(cpgDensity=cpg_density)
+
+    def save_net(self, model):
+        """
+        Save the network to a file
+        :param model: The trained CpGNet model. Located at CpGNet.model
+        :return: Path to the saved model
+        """
+        file_name = "saved_model_{}_cpgs.h5".format(self.cpg_density)
+        output = os.path.join(self.save_path, file_name)
+        model.save(output)
+        print("Saved {} cpg model to {}".format(self.cpg_density, output))
+
+        return output
+
+
+    def train_model(self, bins: iter):
+        """
+        Train the CpGNet model on a list of provided bins
+        :param bins: iterable containing CpG matrices of 1 (methylated), 0 (unmethylated), and -1 (unknown)
+        :return: Path to the saved model file
+        """
+        self.net.train(bins)
+        output = self.save_net(self.net.model)
+
+        return output
+
