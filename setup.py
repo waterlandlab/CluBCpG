6730e086882585e26582f66a7e47a18d3b724a5a
Anthony Scott <ascott329@gmail.com>
2018-11-21 11:51:13 -0600

    new bin file added and version incremented

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="MixtureAnalysis",
-      version="0.1.4",
+      version="0.1.5",
       description="Package to identify epialleles using read clustering from WGBS data",
       author="Anthony Scott, PhD",
       author_email="charles.scott@bcm.edu",
@@ -11,5 +11,6 @@ setup(name="MixtureAnalysis",
                         'seaborn', 'scipy', 'pandas', 'fastcluster'],
       scripts=['bin/CalculateCompleteBins',
                'bin/CombineClusterCompare',
-               'bin/TrainModels'],
+               'bin/TrainModels',
+               'bin/ImputeFromModels'],
       )
