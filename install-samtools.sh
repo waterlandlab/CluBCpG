110981a4f8a1770386b9d6c55a0596675bf10197
Anthony Scott <ascott329@gmail.com>
2019-04-12 10:52:47 -0500

    added installation script for CI

---
+++ install-samtools.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -ex
+wget https://github.com/samtools/samtools/releases/download/1.9/samtools-1.9.tar.bz2
+tar -xzvf samtools-1.9.tar.bz2
+cd samtools-1.9 && ./configure --prefix=/usr && make && sudo make install
+
