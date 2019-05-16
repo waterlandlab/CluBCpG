27aa02a5ee7c0a7a26b2299f41378e06b1e0bc17
Anthony Scott <ascott329@gmail.com>
2019-05-16 15:08:51 -0500

    version updates

--- conf.py
+++ conf.py
@@ -22,9 +22,9 @@ copyright = '2019, Anthony Scott, PhD'
 author = 'Anthony Scott, PhD'
 
 # The short X.Y version
-version = '0.1.9'
+version = '0.1.10'
 # The full version, including alpha,beta,rc tags
-release = '0.1.9'
+release = '0.1.10'
 
 # -- General configuration ---------------------------------------------------
 
