ad2e3e5f405d836854d31498c2d55feff31dabcb
Anthony Scott <ascott329@gmail.com>
2019-07-26 10:04:56 -0500

    doc updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,9 +21,9 @@ project = 'CluBCpG'
 copyright = '2019, Anthony Scott, PhD'
 author = 'Anthony Scott, PhD'
 
-version = '0.1.11'
+version = '0.1.18'
 # The full version, including alpha/beta/rc tags
-release = '0.1.11'
+release = '0.1.18g'
 
 
 # -- General configuration ---------------------------------------------------
